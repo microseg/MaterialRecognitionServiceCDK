c82e55cbd3e09c8b3d7bf355236efb8e7e78e402
Dr-Banana <zhangxuanzhi10419@gmail.com>
2025-09-01 00:59:35 -0700

    feat: switch pipeline to monitor mainline branch

--- bin/main.ts
+++ bin/main.ts
@@ -9,7 +9,7 @@ const app = new cdk.App();
 const githubTokenSecretArn = app.node.tryGetContext('githubTokenSecretArn') || 'arn:aws:secretsmanager:us-east-1:043309364810:secret:github-token-6PK7Gc';
 const githubOwner = app.node.tryGetContext('githubOwner') || 'microseg';
 const githubRepo = app.node.tryGetContext('githubRepo') || 'MaterialRecognitionService';
-const githubBranch = app.node.tryGetContext('githubBranch') || 'master';
+const githubBranch = app.node.tryGetContext('githubBranch') || 'mainline';
 
 new MaterialRecognitionServiceStack(app, 'MaterialRecognitionServiceStack', {
   githubTokenSecretArn,
